c5b29c64503cbe796b7763de28aadc8451330b84
AK391 <ahsengradio@gmail.com>
2021-05-18 13:43:41 -0400

    example

--- gradio/demo.py
+++ gradio/demo.py
@@ -14,7 +14,10 @@ title = "CTRLsum"
 description = "demo for Salesforce CTRLsum. To use it, simply input text or click one of the examples text to load them. Read more at the links below."
 article = "<p style='text-align: center'><a href='https://arxiv.org/abs/2012.04281'>CTRLsum: Towards Generic Controllable Text Summarization</a> | <a href='https://github.com/salesforce/ctrl-sum'>Github Repo</a></p>"
 examples = [
-            ["My name is Kevin. I love dogs. I loved dogs from 1996. Today, I'm going to walk on street with my dogs"]
+            ["""Tunip is the Octonauts' head cook and gardener. 
+He is a Vegimal, a half-animal, half-vegetable creature capable of breathing on land as well as underwater. 
+Tunip is very childish and innocent, always wanting to help the Octonauts in any way he can. 
+He is the smallest main character in the Octonauts crew."""]
 ]
 
 gr.Interface(ctrlsum, inputs, outputs, title=title, description=description, article=article, examples=examples).launch()
